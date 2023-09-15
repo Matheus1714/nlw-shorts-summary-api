4267bf7a5b037c7939a7850fc438068609347a56
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-14 23:10:34 -0300

    update routes

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,41 @@ import cors from 'cors'
 import 'dotenv/config'
 import express, { Request, Response } from 'express'
 import { download } from './download'
+import { transcribe } from './transcribe'
+import { summarize } from './summarize'
+import { convert } from './convert'
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/summary/:id', (request: Request, response: Response) => {
-    const { id } = request.params
+app.get('/summary/:id', async (request: Request, response: Response) => {
+    try{
+        const { id } = request.params
+    
+        await download(id)
+        const audioConverted = await convert()
+    
+        const result = await transcribe(audioConverted)
+        
+        response.json({ result })
 
-    download(id)
+    }catch(error){
+        console.log(error)
+        return response.json({ error })
+    }
+})
 
-    response.json({ result: "Download completed successfully!" })
+app.post('/summary', async (request: Request, response: Response) => {
+    try {
+        const { text } = request.body
+        const result = await summarize(text)
+        return response.json({ result })
+    } catch (error) {
+        console.log(error)
+        return response.json({ error })
+    }
 })
 
 const port = process.env.PORT || 3334
